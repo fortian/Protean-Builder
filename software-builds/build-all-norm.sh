8ed07d52fd412099b32caf9e1237f81dca196153
Bulent Yilmaz <by-github@electromagnetic.net>
2019-07-03 19:39:23 -0400

    Give option of building for particular set of distros

--- software-builds/build-all-norm.sh
+++ software-builds/build-all-norm.sh
@@ -1,41 +1,51 @@
 #!/bin/bash
 
+# Default to building both CentOS and Ubuntu
+CENTOS=${CENTOS:-1}
+UBUNTU=${UBUNTU:-1}
+
 SDIR=$(pwd)
 mkdir -p packages
 
 # First start with CentOS:
 
 if [ "$CENTOS" = 1 ]; then
-(cd ../docker-builds/centos
-
-for i in *; do
-    if [ -d $i ]; then
-        (cd $i
-         CTR=$i-build
-         make start
-         docker cp $SDIR/rpm/norm_build_rpm.sh $CTR:/root/
-         docker exec -i -t $CTR /root/norm_build_rpm.sh
-         docker cp -a $CTR:/root/rpmbuild/RPMS $SDIR/packages
-         docker cp -a $CTR:/root/rpmbuild/SRPMS $SDIR/packages
-         make rmcont
-        )
-    fi
-done
-)
+    (cd ../docker-builds/centos
+    
+    for i in *; do
+        if [ -d $i ]; then
+            (cd $i
+             CTR=$i-build
+             make start
+             docker cp $SDIR/rpm/norm_build_rpm.sh $CTR:/root/
+             docker exec -i -t $CTR /root/norm_build_rpm.sh
+             docker cp -a $CTR:/root/rpmbuild/RPMS $SDIR/packages
+             docker cp -a $CTR:/root/rpmbuild/SRPMS $SDIR/packages
+             make rmcont
+            )
+        fi
+    done
+    )
+else
+    echo "Not building CentOS packages by user request"
 fi
 
 # Next Ubuntu
-(cd ../docker-builds/ubuntu
-for i in *; do
-    if [ -d $i ]; then
-        (cd $i
-         CTR=$i-build
-         make start
-         docker cp $SDIR/deb/norm_build_deb.sh $CTR:/root/
-         docker exec -i -t $CTR /root/norm_build_deb.sh
-         docker cp -a $CTR:/root/PKG/DEB $SDIR/packages
-         make rmcont
-        )
-    fi
-done
-)
+if [ "$UBUNTU" = 1 ]; then
+    (cd ../docker-builds/ubuntu
+    for i in *; do
+        if [ -d $i ]; then
+            (cd $i
+             CTR=$i-build
+             make start
+             docker cp $SDIR/deb/norm_build_deb.sh $CTR:/root/
+             docker exec -i -t $CTR /root/norm_build_deb.sh
+             docker cp -a $CTR:/root/PKG/DEB $SDIR/packages
+             make rmcont
+            )
+        fi
+    done
+    )
+else
+    echo "Not building Ubuntu packages by user request"
+fi
